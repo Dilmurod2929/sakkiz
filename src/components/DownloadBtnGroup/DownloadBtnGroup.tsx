4844be1c21deebe281fb39cf5a66183a616a54a4
Dilmurod <dilmurod8829@gmail.com>
2024-10-02 15:55:10 +0500

    sakkiz-messenger

--- src/components/DownloadBtnGroup/DownloadBtnGroup.tsx
+++ src/components/DownloadBtnGroup/DownloadBtnGroup.tsx
@@ -5,34 +5,53 @@ import Image from "next/image";
 const DownloadBtnGroup = () => {
   return (
     <div className="container">
-      <p className={styles.download_info}>
-        Download Gap on all your devices and enjoy uninterrupted communication.
-      </p>
+      <div className={styles.box}>
+        <p className={styles.download_info}>
+          Download Gap on all your devices and enjoy uninterrupted
+          communication.
+        </p>
+      </div>
       <div className={styles.btns_all}>
         <div className={styles.btns}>
           <a className={styles.download_btn} href="#">
             <Image
+              width={144}
+              height={48}
               src="/images/download-btn-group/app-store.svg"
               alt="App Store"
             />
           </a>
           <a className={styles.download_btn} href="#">
             <Image
+              width={144}
+              height={48}
               src="/images/download-btn-group/googlePlay.svg"
               alt="Google Play"
             />
           </a>
           <a className={styles.download_btn} href="#">
             <Image
+              width={144}
+              height={48}
               src="/images/download-btn-group/appGallery.svg"
               alt="App Gallery"
             />
           </a>
           <a className={styles.download_btn} href="#">
-            <Image src="/images/download-btn-group/macOs.svg" alt="macOS" />
+            <Image
+              width={144}
+              height={48}
+              src="/images/download-btn-group/macOs.svg"
+              alt="macOS"
+            />
           </a>
           <a className={styles.download_btn} href="#">
-            <Image src="/images/download-btn-group/windows.svg" alt="Windows" />
+            <Image
+              width={144}
+              height={48}
+              src="/images/download-btn-group/windows.svg"
+              alt="Windows"
+            />
           </a>
         </div>
       </div>
