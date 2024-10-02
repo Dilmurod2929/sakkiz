1fc467d5483db549fb0b62620d628ddcebd81268
Dilmurod <dilmurod8829@gmail.com>
2024-10-02 14:39:37 +0500

    debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  experimental: {
+    outputStandalone: true,
+  },
+};
 
 export default nextConfig;
